4d88189ec838db471e8eebd81f35076cb4109f05
Vaconcovat <vaconcovat@gmail.com>
2016-05-25 19:30:20 +1000

    New Music!

--- Assets/GolbalSounds.cs
+++ Assets/GolbalSounds.cs
@@ -5,10 +5,12 @@ public class GolbalSounds : MonoBehaviour {
 	public AudioSource startup;
 	public AudioSource music;
 	public AudioSource fail;
+	bool played = false;
 
 	public void StartMusic(){
-		if(!music.isPlaying){
+		if(!music.isPlaying && !played){
 			music.Play();
+			played = true;
 		}
 	}
 }
--- Assets/OrbitingText.cs
+++ Assets/OrbitingText.cs
@@ -4,16 +4,17 @@ using System.Collections;
 public class OrbitingText : MonoBehaviour {
 	public Transform gravity;
 	public float speed;
-
+	GameStats gs;
 	public float alpha;
 
 	// Use this for initialization
 	void Start () {
+		gs = FindObjectOfType<GameStats>();
 		speed += (Random.Range(0,5));
 		if(Random.value > 0.5f){
 			speed = -speed;
 		}
-		transform.position = new Vector3(transform.position.x, transform.position.y + Random.Range(-3,3),transform.position.z);
+		transform.position = new Vector3(transform.position.x, transform.position.y + Random.Range(-1,3),transform.position.z);
 	}
 	
 	// Update is called once per frame
@@ -22,9 +23,15 @@ public class OrbitingText : MonoBehaviour {
 		transform.LookAt(Camera.main.transform.position);
 		alpha = 1 - (FindObjectOfType<GameStats>().levelTime / 45);
 		GetComponent<MeshRenderer>().material.color = new Color(1,1,1,alpha);
+
+		if(gs.levelTime < 20){
+			if(speed > 0){
+				speed += Time.deltaTime * 4;
+			}
+			else{
+				speed -= Time.deltaTime * 4;
+			}
+		}
 	}
 
-	void OnDrawGizmos(){
-		//Gizmos.DrawSphere(gravity.position, Vector3.Distance(transform.position, gravity.position));
-	}
 }
