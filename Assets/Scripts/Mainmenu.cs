47207188e7433fcb9a9d5e0539e161323cf55053
Markrot <volkmann1996@hotmail.com>
2016-05-26 09:26:29 +1000

    menu mouse show
    shows mouse on the menu

--- Assets/Scripts/Mainmenu.cs
+++ Assets/Scripts/Mainmenu.cs
@@ -10,6 +10,7 @@ public class Mainmenu : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		//GetComponent<Renderer> ().material.color = Color.white;
+		Cursor.visible = true;
 	}
 
 	// Update is called once per frame
